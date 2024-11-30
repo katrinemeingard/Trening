1b749b3738845fd0471aae49e4b5b84ff79f70fc
Katrine Meingard <katrinemeingard@gmail.com>
2024-11-30 21:26:36 +0300

    Update module_3_calls.py

--- module_3_calls.py
+++ module_3_calls.py
@@ -1,28 +1,34 @@
 calls = 0
-def count_calls():   #подсчитывающая вызовы остальных функций
-    calls =+ 1          #Эта функция должна вызываться в остальных двух функциях.
-    print(calls)
+
+def count_calls():
+    global calls
+    calls = calls + 1
+    return calls
 
 def string_info(string, a=None):
+    global calls
+    calls = calls + 1
     a = []
     a.append(len(string))
     a.append(string.upper())
     a.append(string.lower())
-    tuple(a)
-    print(a)
-    print(count_calls())
+    return a
+#count_calls()
 
-def is_contains(s, list=[]):
+def is_contains(s, list):
+    global calls
+    calls = calls + 1
     string = s.lower()
     list_to_search = [i.lower() for i in list]
     if any(string in i for i in list_to_search):
-        print(True)
+        m = True
     else:
-        print(False)
-    print(count_calls())
+        m = False
+    return m
+#count_calls()
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
 print(calls)
