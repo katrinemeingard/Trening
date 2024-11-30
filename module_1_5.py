069ecd23e3448ff69c0a53e506d412c83d15ea2b
katrinemeingard <katrinemeingard@gmail.com>
2024-11-30 12:48:03 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = (1, 'finger', 7, 6, 'unreal', [4,2,3,7,8])
+print(immutable_var)
+immutable_var[5][4] = 9
+print(immutable_var)
+mutable_list = ['simple', 38, 2, 9]
+print(mutable_list)
+mutable_list[0] = 'sample'
+mutable_list[3] = 54
+print(mutable_list)
+
+print("QQEPTA!")
